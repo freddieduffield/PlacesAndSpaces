a8ade5c4a674d6bae581b54aab8dbca96c01c561
FrederickFoxxx <fwhduffield@gmail.com>
2019-10-03 07:30:26 +0100

    queries, mutations, subscriptions with amplify and appsync

--- App.js
+++ App.js
@@ -8,43 +8,88 @@
 
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
-  Text,
   StatusBar,
+  Text,
+  TextInput,
+  View,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
 
 import {API, graphqlOperation} from 'aws-amplify';
-import {listRestaurants} from './src/graphql/queries';
 import {ApiGatewayManagementApi} from 'aws-sdk/clients/all';
+import uuid from 'uuid/v4';
+
+import {listRestaurants} from './src/graphql/queries';
+import {createRestaurant} from './src/graphql/mutations';
+import {onCreateRestaurant} from './src/graphql/subscriptions';
+
+const CLIENTID = uuid();
 
 class App extends React.Component {
   state = {
+    name: '',
+    description: '',
+    city: '',
     restaurants: [],
   };
-  async componentDidMount() {
-    try {
-      const restaurantData = await API.graphql(
-        graphqlOperation(listRestaurants),
-      );
-      console.log('restaurantData: ', restaurantData);
-      this.setState({
-        restaurants: restaurantData.data.listRestaurants.items,
-      });
-    } catch (err) {
-      console.log('error fetching restaurants...', err);
-    }
+
+  subscription = {};
+
+  componentDidMount() {
+    this.subscription = API.graphql(
+      graphqlOperation(onCreateRestaurant),
+    ).subscribe({
+      next: eventData => {
+        console.log('eventData', eventData);
+        const restaurant = eventData.value.data.onCreateRestaurant;
+        if (CLIENTID === restaurant.clientId) return;
+        const restaurants = [...this.state.restaurants, restaurant];
+        this.setState({restaurants});
+      },
+    });
   }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
+
+  createRestaurant = async () => {
+    const {name, description, city} = this.state;
+    const restaurant = {
+      name,
+      description,
+      city,
+      clientId: CLIENTID,
+    };
+
+    const updatedRestaurantArray = [...this.state.restaurants, restaurant];
+    this.setState({
+      restaurants: updatedRestaurantArray,
+      name: '',
+      description: '',
+      city: '',
+    });
+
+    try {
+      await API.graphql(
+        graphqlOperation(createRestaurant, {
+          input: restaurant,
+        }),
+      );
+      console.log('item created');
+    } catch (err) {
+      console.log('error creating restaurant...', err);
+    }
+  };
+
+  onChange = (key, value) => {
+    this.setState({[key]: value});
+  };
 
   render() {
     return (
@@ -54,6 +99,7 @@ class App extends React.Component {
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
+            <Header />
             {this.state.restaurants.map((restaurant, index) => (
               <View key={index}>
                 <Text>{restaurant.name}</Text>
@@ -61,12 +107,27 @@ class App extends React.Component {
                 <Text>{restaurant.city}</Text>
               </View>
             ))}
-            <Header />
             {global.HermesInternal == null ? null : (
               <View style={styles.engine}>
                 <Text style={styles.footer}>Engine: Hermes</Text>
               </View>
             )}
+            <TextInput
+              onChangeText={v => this.onChange('name', v)}
+              value={this.state.name}
+              style={{height: 50, margin: 5, backgroundColor: '#ddd'}}
+            />
+            <TextInput
+              onChangeText={v => this.onChange('description', v)}
+              value={this.state.description}
+              style={{height: 50, margin: 5, backgroundColor: '#ddd'}}
+            />
+            <TextInput
+              onChangeText={v => this.onChange('city', v)}
+              value={this.state.city}
+              style={{height: 50, margin: 5, backgroundColor: '#ddd'}}
+            />
+            <Button onPress={this.createRestaurant} title="Create Restaurant" />
           </ScrollView>
         </SafeAreaView>
       </>
@@ -110,6 +171,11 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
   },
 });
 
---
+++ src/readme.md
@@ -0,0 +1,15 @@
+# Places and Spaces
+
+Run the app
+
+    react-native run ios
+
+## Todo
+
+---
+
+- add prettier eslint config
+
+- Write up amplify commands.
+
+- mobile app that writes markdown
